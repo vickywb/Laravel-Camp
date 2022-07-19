b5b0dd8a1f7537f866bda741a40980929663185b
vicky wibisono <wibivicky@gmail.com>
2022-07-19 19:16:36 +0700

    fixing typo in migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->sring('occupation')->nullable();
+            $table->string('occupation')->nullable();
             $table->boolean('role')->default(false);
             $table->softDeletes();
             $table->rememberToken();
