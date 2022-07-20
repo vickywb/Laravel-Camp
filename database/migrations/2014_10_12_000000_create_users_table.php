fc4ee6b4e739585dbe2d46ffaf7bacbe03abae38
vicky wibisono <wibivicky@gmail.com>
2022-07-20 08:29:36 +0700

    change name role to is_admin in model and migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('occupation')->nullable();
-            $table->boolean('role')->default(false);
+            $table->boolean('is_admin')->default(false);
             $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
