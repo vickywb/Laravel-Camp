91baa1eb5f99a8583a28092aa086c6c42a1d1496
vicky wibisono <wibivicky@gmail.com>
2022-09-01 07:00:43 +0700

    fixing checkout blade

--- resources/views/pages/checkouts/checkout.blade.php
+++ resources/views/pages/checkouts/checkout.blade.php
@@ -20,7 +20,7 @@
                         <div class="item-bootcamp">
                             <img src="{{ asset('images/item_bootcamp.png') }}" alt="" class="cover">
                             <h1 class="package">
-                                GILA BELAJAR
+                                {{ $camp->title }}
                             </h1>
                             <p class="description">
                                 Bootcamp ini akan mengajak Anda untuk belajar penuh mulai dari pengenalan dasar sampai membangun sebuah projek asli
@@ -29,32 +29,57 @@
                     </div>
                     <div class="col-lg-1 col-12"></div>
                     <div class="col-lg-6 col-12">
-                        <form action="{{ route('success-checkout') }}" class="basic-form">
+                        <form action="{{ route('checkout.store', $camp) }}" class="basic-form" method="POST">
+                            @csrf
                             <div class="mb-4">
-                                <label for="exampleInputEmail1" class="form-label">Full Name</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <label for="InputName" class="form-label">Full Name</label>
+                                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" id="InputName" value="{{ old('name') }}">
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                             <div class="mb-4">
-                                <label for="exampleInputEmail1" class="form-label">Email Address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <label for="InputEmail1" class="form-label">Email Address</label>
+                                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="InputEmail" value="{{ old('email') }}">
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                             <div class="mb-4">
-                                <label for="exampleInputEmail1" class="form-label">Occupation</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            </div>
-                            <div class="mb-4">
-                                <label for="exampleInputEmail1" class="form-label">Card Number</label>
-                                <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <label for="InputCardNumber" class="form-label">Card Number</label>
+                                <input type="text" pattern="\d*" maxlength="20" name="card_number" class="form-control @error('card_number') is-invalid @enderror" id="InputCardNumber" value="{{ old('card_number') }}">
+                                @error('card_number')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                             <div class="mb-5">
                                 <div class="row">
                                     <div class="col-lg-6 col-12">
-                                        <label for="exampleInputEmail1" class="form-label">Expired</label>
-                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                        <label for="datepicker" class="form-label">Expired</label>
+                                        <div class="input-append date" id="datepicker">
+                                            <input type="text" name="expired_date" class="form-control @error('expired_date') is-invalid @enderror">
+                                            <span class="add-on"></span>
+                                            @error('expired_date')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
                                     </div>
                                     <div class="col-lg-6 col-12">
-                                        <label for="exampleInputEmail1" class="form-label">CVC</label>
-                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                        <label for="InputCVC" class="form-label">CVC</label>
+                                        <input type="text" pattern="\d*" maxlength="3" name="cvc" class="form-control @error('cvc') is-invalid @enderror" id="InputCVC">
+                                        @error('cvc')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
                                     </div>
                                 </div>
                             </div>
@@ -69,4 +94,21 @@
         </div>
     </div>
 </section>
+@endsection
+@section('javascript')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" rel="stylesheet" />
+<script type="text/javascript">
+    const dateNow = new Date();
+    let date = dateNow.getMonth();
+    date += 1
+    
+    $("#datepicker").datepicker( {
+        format: 'mm-yyyy',
+        startView: 'months', 
+        minViewMode: 'months',
+        startDate: `${date}`,
+    });
+</script>
 @endsection
